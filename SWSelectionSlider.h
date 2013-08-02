49b5ddde1423858663902e6d8d183999960bd334
Sam Watson <sam@toomuch.com.au>
2013-08-03 04:59:56 +0800

    Initial commit

---
+++ SWSelectionSlider.h
@@ -0,0 +1,13 @@
+//
+//  SWSelectionSlider.h
+//  
+//
+//  Created by Sam Watson on 3/08/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SWSelectionSlider : UIView
+
+@end
---
+++ SWSelectionSlider.m
@@ -0,0 +1,31 @@
+//
+//  SWSelectionSlider.m
+//  
+//
+//  Created by Sam Watson on 3/08/13.
+//
+//
+
+#import "SWSelectionSlider.h"
+
+@implementation SWSelectionSlider
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
