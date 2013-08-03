24751f73c10c29cb5115921dc8e079eb92498162
Sam Watson <sam@toomuch.com.au>
2013-08-03 15:09:44 +0800

    Added delegate callbacks

--- SWSelectionSlider.h
+++ SWSelectionSlider.h
@@ -8,6 +8,21 @@
 
 #import <UIKit/UIKit.h>
 
+@class SWSelectionSlider;
+
+@protocol SWSelectionSliderDataSource <NSObject>
+
+- (NSInteger)numberOfSelectionsForSlider:(SWSelectionSlider *)slider;
+- (NSString *)slider:(SWSelectionSlider *)slider titleForSelectionAtIndex:(NSInteger)index;
+
+@end
+
+@protocol SWSelectionSliderDelegate <NSObject>
+
+- (void)slider:(SWSelectionSlider *)slider didSelectIndex:(NSInteger)index;
+
+@end
+
 @interface SWSelectionSlider : UIView
 
 @end
